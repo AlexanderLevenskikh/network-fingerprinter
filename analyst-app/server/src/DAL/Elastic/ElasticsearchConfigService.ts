9f0d2ce626c5b1d6d62b003ce67d7ec2d3851281
AlexanderLevenskikh <a.levenskikh@gmail.com>
2019-12-31 04:42:27 +0500

    Fix dockerfile, add to compose

--- analyst-app/server/src/DAL/Elastic/ElasticsearchConfigService.ts
+++ analyst-app/server/src/DAL/Elastic/ElasticsearchConfigService.ts
@@ -3,7 +3,7 @@ import { ElasticsearchModuleOptions, ElasticsearchOptionsFactory } from '@nestjs
 export class ElasticsearchConfigService implements ElasticsearchOptionsFactory {
     createElasticsearchOptions(): ElasticsearchModuleOptions {
         return {
-            host: 'http://192.168.1.50:9200',
+            host: 'http://127.0.0.1:9200',
             httpAuth: 'elastic:12345',
         };
     }
