dd1446d0e1acf64d2496f94b0e0c9bf4a357d690
AlexanderLevenskikh <a.levenskikh@gmail.com>
2020-01-08 23:08:11 +0500

    Fix script reserved names

--- analyst-app/db/init.sql
+++ analyst-app/db/init.sql
@@ -1,13 +1,13 @@
-CREATE TABLE user (
+CREATE TABLE "user" (
     userId uuid PRIMARY KEY NOT NULL,
-    name varchar(100) NOT NULL,
+    userName varchar(100) NOT NULL,
     firstName varchar(100),
     lastName varchar(100),
     middleName varchar(100),
-    passwordHash varchar(255) NOT NULL
-    admin boolean NOT NULL
+    passwordHash varchar(255) NOT NULL,
+    isAdmin boolean NOT NULL
 );
 
-INSERT INTO user (userId, name, passwordHash, admin)
+INSERT INTO "user" (userId, userName, passwordHash, isAdmin)
 SELECT '00000000-0000-0000-0000-000000000000', 'root', '63a9f0ea7bb98050796b649e85481845', true
-WHERE NOT EXISTS (SELECT * FROM user)
+WHERE NOT EXISTS (SELECT * FROM "user")
